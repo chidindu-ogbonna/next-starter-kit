e976f5328fbf1c0d49fd16e63abd37947dc1dc5b
bones <promise.bones@gmail.com>
2022-02-13 11:30:18 +0100

    install sass & headlessui

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  images: {
+    domains: ["tailwindui.com", "images.unsplash.com", "picsum.photos"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
