466f31601785006353546ea5b735281e138d9f31
bones <promise.bones@gmail.com>
2022-02-13 17:22:20 +0100

    Include metadata file

---
+++ data/metadata.js
@@ -0,0 +1,17 @@
+export const metadata = {
+  siteName: "NextJS Starter Kit",
+  title: "NextJS Starter Kit",
+  author: "6ones",
+  description: "Everything you need in a nextjs project",
+  language: "en-us",
+  siteUrl: "https://whoisbones.xyz",
+  siteImageURL:
+    "https://res.cloudinary.com/cheapflix/image/upload/v1591556042/coronavirus_1.png",
+  favicon: "/favicon.png",
+  email: "address@yoursite.com",
+  locale: "en-US",
+  analytics: {
+    googleAnalyticsId: "", // e.g. UA-000000-2 or G-XXXXXXX
+  },
+  twitter: "6ones_",
+};
